f24199cbcd6f9364bd6c93d5978b82deda31be87
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-05-08 05:47:40 +0300

    update RunnerKt

--- src/main/kotlin/benchmarks/Runner.kt
+++ src/main/kotlin/benchmarks/Runner.kt
@@ -11,12 +11,15 @@ fun main(args: Array<String>) {
     val outputFile = "teamcityArtifacts/$implementation.csv"
     val options = OptionsBuilder()
             .jvmArgs("-Xms3072m", "-Xmx3072m")
-            .include("getByIndex")
-            .include("setByIndex")
+//            .include("addLast$")
+//            .include("getByIndex")
+//            .include("setByIndex")
+//            .include("firstToLast")
             .warmupIterations(10)
             .measurementIterations(10)
             .warmupTime(TimeValue.milliseconds(2000))
             .measurementTime(TimeValue.milliseconds(2000))
+//            .param("listSize", BM_1000000/*, BM_10000000*/)
             .addProfiler("gc")
 
     val runResults = Runner(options.build()).run()
@@ -24,7 +27,7 @@ fun main(args: Array<String>) {
 }
 
 fun printResults(runResults: Collection<RunResult>, implementation: String, outputFile: String) {
-    val csvHeader = "Implementation,Method,listSize,heightType,bufferType,bufferSize,Score,Score Error,Allocation Rate"
+    val csvHeader = "Implementation,Method,listSize,Score,Score Error,Allocation Rate"
 
     val fileWriter = FileWriter(outputFile)
 
@@ -39,29 +42,12 @@ fun printResults(runResults: Collection<RunResult>, implementation: String, outp
 }
 
 fun csvRowFrom(result: RunResult, implementation: String): String {
-    val nanosInMillis = 1000
+    val nanosInMicros = 1000
     val method = result.primaryResult.getLabel()
     val listSize = result.params.getParam("listSize").toInt()
-    val score = result.primaryResult.getScore() * nanosInMillis / listSize
-    val scoreError = result.primaryResult.getScoreError() * nanosInMillis / listSize
+    val score = result.primaryResult.getScore() * nanosInMicros / listSize
+    val scoreError = result.primaryResult.getScoreError() * nanosInMicros / listSize
     val allocationRate = result.secondaryResults["·gc.alloc.rate.norm"]!!.getScore() / listSize
 
-    val impl = result.params.getParam("impl")
-    val (heightType, bufferType, bufferSize) = extractInfo(impl)
-
-    return "$implementation,$method,$listSize,$heightType,$bufferType,$bufferSize,%.3f,%.3f,%.3f"
-            .format(score, scoreError, allocationRate)
-}
-
-fun extractInfo(impl: String): Triple<String, String, String> {
-    val lastDelimiter = impl.indexOfLast { it == '_' }
-    val bufferSize = impl.substring(lastDelimiter + 1)
-
-    if (impl.startsWith("FIXED_H_")) {
-        val delimiter = impl.substring(8).indexOfFirst { it == '_' }
-        return Triple("FIXED", impl.substring(8, 8 + delimiter), bufferSize)
-    }
-    assert(impl.startsWith("VARIABLE_H_"))
-    val delimiter = impl.substring(11).indexOfFirst { it == '_' }
-    return Triple("VARIABLE", impl.substring(11, 11 + delimiter), bufferSize)
+    return "$implementation,$method,$listSize,%.3f,%.3f,%.3f".format(score, scoreError, allocationRate)
 }
